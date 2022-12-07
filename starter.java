e7d9791e7d842ad91a2ce07e41ae623e2df419fb
Josh K <116848707+JoshSK2331@users.noreply.github.com>
2022-12-07 15:12:53 -0800

    Update starter.java

--- starter.java
+++ starter.java
@@ -1,18 +1,22 @@
+import java.util.ArrayList;
+
 class starter{
 	public static void main(String args[]) {
 		int C=2;
+		ArrayList<Integer> Infi=new ArrayList<Integer>();
+		Infi.add(2);
 		boolean Prime=true;
 		String Numbers=new String();
 		String List=new String();
 		int I=0;
-		System.out.println("\nPlease wait...");
 		while(C<2147483647){
-			for(int Counter=2;Counter<C;Counter++){
-				if(C%Counter==0){
+			for(int Counter=2;Counter<Infi.size();Counter++){
+				if(C%Infi.get(Counter)==0){
 					Prime=false;
 				}
 			}
 			if(Prime){
+				Infi.add(C);
 				List=new String(C+" ");
 				Numbers+=List;
 				I++;
